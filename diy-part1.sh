6b9771fdafb08d2def0a7c5a19fc9872490a61d7
Xghng <135593952+Xghng@users.noreply.github.com>
2024-11-21 13:16:52 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -9,7 +9,7 @@
 #=============================================================
 
 ### 添加第三方订阅源
-#sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 ### 定时限速插件
 sed -i '$a src-git eqosplus  https://github.com/sirpdboy/luci-app-eqosplus' feeds.conf.default
