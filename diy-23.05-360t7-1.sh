329901317c452fb38d50279f069a2928a6ab9525
Xghng <135593952+Xghng@users.noreply.github.com>
2024-12-01 19:40:38 +0800

    Update diy-23.05-360t7-1.sh

--- diy-23.05-360t7-1.sh
+++ diy-23.05-360t7-1.sh
@@ -1,1 +1,15 @@
+#!/bin/bash
+#=============================================================
+# https://github.com/P3TERX/Actions-OpenWrt
+# File name: diy-part1.sh
+# Description: OpenWrt DIY script part 1 (Before Update feeds)
+# Lisence: MIT
+# Author: P3TERX
+# Blog: https://p3terx.com
+#=============================================================
 
+### 添加第三方订阅源
+sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
+# echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+# sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
+# sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
