408e5f0fd7cb2ee4429e80ce0bb7b96645d83904
Xghng <135593952+Xghng@users.noreply.github.com>
2024-12-04 18:06:57 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -13,3 +13,4 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 # sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
+git clone https://github.com/destan19/OpenAppFilter.git package/OpenAppFilter
