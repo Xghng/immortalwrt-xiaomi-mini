76272e9e1f3835820c432faac81f1074c7423f14
Xghng <135593952+Xghng@users.noreply.github.com>
2024-12-05 22:36:46 +0800

    Update diy-part1-AP-360t7.sh

--- 360T7/immortalwrt23.05/AP-360t7/diy-part1-AP-360t7.sh
+++ 360T7/immortalwrt23.05/AP-360t7/diy-part1-AP-360t7.sh
@@ -13,7 +13,7 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 # sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
-### 应用过滤插件
+### 应用过滤luci-app-oaf插件
 cd lede
 sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
 ./scripts/feeds update -a && ./scripts/feeds install -a
