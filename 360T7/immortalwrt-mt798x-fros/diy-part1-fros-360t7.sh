649e5d10480f4db824bb75dce00f1420ec9422be
Xghng <135593952+Xghng@users.noreply.github.com>
2024-12-06 09:20:13 +0800

    Update diy-part1-fros-360t7.sh

--- 360T7/immortalwrt-mt798x-fros/diy-part1-fros-360t7.sh
+++ 360T7/immortalwrt-mt798x-fros/diy-part1-fros-360t7.sh
@@ -13,8 +13,10 @@
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 # sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
+### fros防沉迷系统
+sed -i '$a src-git-full fros https://github.com/destan19/fros-packages-immortalwrt-mt798x.git' feeds.conf.default
 ### 应用过滤luci-app-oaf插件
-cd lede
-sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
-./scripts/feeds update -a && ./scripts/feeds install -a
+### cd lede
+### sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
+### ./scripts/feeds update -a && ./scripts/feeds install -a
 #
