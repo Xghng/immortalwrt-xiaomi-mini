9d8a8799cc9786518087abb846e17f189e5da571
Xghng <135593952+Xghng@users.noreply.github.com>
2024-12-06 21:13:26 +0800

    Update diy-part1-homeproxy.sh

--- 360T7/immortalwrt23.05/homeproxy/diy-part1-homeproxy.sh
+++ 360T7/immortalwrt23.05/homeproxy/diy-part1-homeproxy.sh
@@ -15,7 +15,7 @@ sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.co
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 # sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
 ### 应用过滤插件
-# cd lede
-# sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
-# ./scripts/feeds update -a && ./scripts/feeds install -a
+cd lede
+sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
+./scripts/feeds update -a && ./scripts/feeds install -a
 #
