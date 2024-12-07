6e119cac4758d7ea843fb179c744289547895236
Xghng <135593952+Xghng@users.noreply.github.com>
2024-12-07 20:58:51 +0800

    Update diy-part1-oaf.sh

--- xiaomi-mini/immortalwrt-21.02/oaf/diy-part1-oaf.sh
+++ xiaomi-mini/immortalwrt-21.02/oaf/diy-part1-oaf.sh
@@ -9,11 +9,11 @@
 #=============================================================
 
 ### 添加第三方订阅源
-# sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
 # sed -i '$a src-git-full small https://github.com/Xghng/kenzok8-small-package' feeds.conf.default
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 # sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
-cd lede
-sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
-./scripts/feeds update -a && ./scripts/feeds install -a
+# cd lede
+# sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
+# ./scripts/feeds update -a && ./scripts/feeds install -a
