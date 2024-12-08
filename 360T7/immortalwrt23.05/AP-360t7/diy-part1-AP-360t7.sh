7d5ccd935532c2778a762272188227a2d367cc88
Xghng <135593952+Xghng@users.noreply.github.com>
2024-12-08 20:45:43 +0800

    Update diy-part1-AP-360t7.sh

--- 360T7/immortalwrt23.05/AP-360t7/diy-part1-AP-360t7.sh
+++ 360T7/immortalwrt23.05/AP-360t7/diy-part1-AP-360t7.sh
@@ -14,7 +14,7 @@
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 # sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
 ### openwrt在线设备查看luci-app-onliner
-sed -i '$a src-git-full onliner https://github.com/Xghng/luci-app-onliner.git' feeds.conf.default
+sed -i '$a src-git-full wrtbwmon https://github.com/Xghng/luci-app-wrtbwmon.git' feeds.conf.default
 ### 应用过滤luci-app-oaf插件
-sed -i '$a src-git OpenAppFilter https://github.com/Xghng/OpenAppFilter.git' feeds.conf.default
+sed -i '$a src-git-full OpenAppFilter https://github.com/Xghng/OpenAppFilter.git' feeds.conf.default
 #
