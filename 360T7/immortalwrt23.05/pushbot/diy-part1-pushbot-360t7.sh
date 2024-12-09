2160f53c63e2c89adf3b30dae37028e870a0a7b2
Xghng <135593952+Xghng@users.noreply.github.com>
2024-12-09 16:08:47 +0800

    Update diy-part1-pushbot-360t7.sh

--- 360T7/immortalwrt23.05/pushbot/diy-part1-pushbot-360t7.sh
+++ 360T7/immortalwrt23.05/pushbot/diy-part1-pushbot-360t7.sh
@@ -14,5 +14,5 @@ sed -i '$a src-git-full small https://github.com/Xghng/kenzok8-small-package' fe
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 # sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
 ### 应用过滤luci-app-oaf插件
-# sed -i '$a src-git-full OpenAppFilter https://github.com/Xghng/OpenAppFilter.git' feeds.conf.default
+sed -i '$a src-git-full OpenAppFilter https://github.com/Xghng/OpenAppFilter.git' feeds.conf.default
 #
