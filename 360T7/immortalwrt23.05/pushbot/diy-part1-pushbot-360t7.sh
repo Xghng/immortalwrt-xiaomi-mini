310975300a958697e5b3bf69f7d48111918a5f9e
Xghng <135593952+Xghng@users.noreply.github.com>
2024-12-09 14:12:08 +0800

    Update diy-part1-pushbot-360t7.sh

--- 360T7/immortalwrt23.05/pushbot/diy-part1-pushbot-360t7.sh
+++ 360T7/immortalwrt23.05/pushbot/diy-part1-pushbot-360t7.sh
@@ -9,14 +9,14 @@
 #=============================================================
 
 ### 添加第三方订阅源
-# sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
+sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 # sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
 ### 本插件支持：钉钉推送,企业微信推送,PushPlus推送.
-sed -i '$a src-git-full clone https://github.com/zzsj0928/luci-app-pushbot.git' feeds.conf.default
+# sed -i '$a src-git-full clone https://github.com/zzsj0928/luci-app-pushbot.git' feeds.conf.default
 ### openwrt在线设备查看luci-app-onliner
 # sed -i '$a src-git-full wrtbwmon https://github.com/Xghng/luci-app-wrtbwmon.git' feeds.conf.default
 ### 应用过滤luci-app-oaf插件
-sed -i '$a src-git-full OpenAppFilter https://github.com/Xghng/OpenAppFilter.git' feeds.conf.default
+# sed -i '$a src-git-full OpenAppFilter https://github.com/Xghng/OpenAppFilter.git' feeds.conf.default
 #
