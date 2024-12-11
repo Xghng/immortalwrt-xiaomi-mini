2f90198b07c8789e0fb304d306584f58e16c1eed
Xghng <135593952+Xghng@users.noreply.github.com>
2024-12-11 16:08:48 +0800

    Update diy-part1-ap.sh

--- xiaomi-mini/immortalwrt-21.02/xiaomi-mini-ap/diy-part1-ap.sh
+++ xiaomi-mini/immortalwrt-21.02/xiaomi-mini-ap/diy-part1-ap.sh
@@ -9,7 +9,7 @@
 #=============================================================
 
 ### 添加第三方订阅源
-sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
+# sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
 # sed -i '$a src-git-full small https://github.com/Xghng/kenzok8-small-package' feeds.conf.default
 # sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
 # sed -i '$a src-git NueXini_Packages https://github.com/Xghng/NueXini_Packages.git' feeds.conf.default
@@ -17,6 +17,6 @@ sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.co
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 # sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
-# cd lede
-# sed -i '$a src-git NueXini_Packages https://github.com/Xghng/NueXini_Packages.git' feeds.conf.default
-# ./scripts/feeds update -a && ./scripts/feeds install -a
+cd lede
+sed -i '$a src-git NueXini_Packages https://github.com/Xghng/NueXini_Packages.git' feeds.conf.default
+./scripts/feeds update -a && ./scripts/feeds install -a
