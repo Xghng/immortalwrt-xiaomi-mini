ee718ada9dd87b807c3d2645eb9848271806a468
Xghng <135593952+Xghng@users.noreply.github.com>
2024-12-11 15:10:49 +0800

    Update diy-part1-ap.sh

--- xiaomi-mini/immortalwrt-21.02/xiaomi-mini-ap/diy-part1-ap.sh
+++ xiaomi-mini/immortalwrt-21.02/xiaomi-mini-ap/diy-part1-ap.sh
@@ -11,7 +11,8 @@
 ### 添加第三方订阅源
 # sed -i '$a src-git-full small https://github.com/kenzok8/small-package' feeds.conf.default
 # sed -i '$a src-git-full small https://github.com/Xghng/kenzok8-small-package' feeds.conf.default
-echo 'src-git control-weburl https://github.com/Xghng/luci-app-control-weburl' feeds.conf.default
+sed -i '$a src-git NueXini_Packages https://github.com/NueXini/NueXini_Packages.git' feeds.conf.default
+# echo 'src-git control-weburl https://github.com/Xghng/luci-app-control-weburl' feeds.conf.default
 # echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 # sed -i '$a src-git passwall https://github.com/xiaorouji/openwrt-passwall' feeds.conf.default
 # sed -i '$a src-git helloworld https://github.com/P3TERX/helloworld' feeds.conf.default
